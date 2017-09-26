4ff6338dccb81e590aebf5a008fe58445c4455a7
Lukasz Bany <lbany89@gmail.com>
2017-09-26 09:04:36 +0200

    initial commit

---
+++ src/main/java/com/lukasz/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.lukasz;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return  "Hello world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
---
+++ src/test/java/com/lukasz/HelloWorldControllerTest.java
@@ -0,0 +1,7 @@
+package com.lukasz;
+
+import junit.framework.TestCase;
+
+public class HelloWorldControllerTest extends TestCase {
+
+}
